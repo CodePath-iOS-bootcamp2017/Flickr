50aa70c3d28bc70ef03e285a49c80fff53346246
Satyam Jaiswal <snjaiswa@asu.edu>
2017-02-06 07:41:07 -0700

    code for handling filter events and minor UI tweaks

--- Flickr/MoviesViewController.swift
+++ Flickr/MoviesViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 import AFNetworking
 import SVProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate{
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
-    
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var movieDictionary: [NSDictionary]?
+    var filteredMovies: [NSDictionary]?
     var errorCode = 0
     let button = UIButton()
     let refreshControl = UIRefreshControl()
@@ -31,6 +32,7 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
         
+        self.searchBar.delegate = self
         
         button.frame = CGRect(x: 0, y: 0, width: 35, height: 25)
         button.setImage(UIImage(named:"listView.png"), for: UIControlState.normal)
@@ -61,8 +63,9 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // Dispose of any resources that can be recreated.
     }
     
+    // overriding methods for table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let movieCount = movieDictionary?.count{
+        if let movieCount = filteredMovies?.count{
             return movieCount
         }else{
             return 0
@@ -71,7 +74,7 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as? MovieTableViewCell
-        if let movie = movieDictionary?[indexPath.row]{
+        if let movie = filteredMovies?[indexPath.row]{
             cell?.titleLabel.text = movie["original_title"] as? String
             cell?.overviewLabel.text = movie["overview"] as? String
             
@@ -83,23 +86,6 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
         
         return cell!
-    }
-    
-    func fadeInImageAtView(url: URL, posterImageView: UIImageView) -> Void{
-        let imageRequest = URLRequest(url: url)
-        posterImageView.setImageWith(imageRequest, placeholderImage: nil, success: { (imageRequest, response, image) in
-            if response != nil{
-                posterImageView.alpha = 0
-                posterImageView.image = image
-                UIView.animate(withDuration: 0.3, animations: {
-                    posterImageView.alpha = 1
-                })
-            }else{
-                posterImageView.image = image
-            }
-        }) { (imageRequest, response, error) in
-            print(error)
-        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -126,8 +112,9 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
     
+    // overriding methods for collection view
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if let movieCount = movieDictionary?.count{
+        if let movieCount = filteredMovies?.count{
             return movieCount
         }else{
             return 0
@@ -137,7 +124,7 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as! MovieCollectionViewCell
             
-        if let movie = movieDictionary?[indexPath.row]{
+        if let movie = filteredMovies?[indexPath.row]{
             let baseURL = "https://image.tmdb.org/t/p/w500"
             let filePath = movie["poster_path"] as? String
             let posterURL = URL(string: baseURL+filePath!)
@@ -177,6 +164,26 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
     
+    // overriding methods for search bar
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if(searchText.isEmpty){
+//            print("isEmpty")
+            self.filteredMovies = self.movieDictionary
+        }else{
+//            print("not Empty")
+            self.filteredMovies = self.movieDictionary?.filter({ (movie) -> Bool in
+                if let title = movie.value(forKey: "original_title") as? String{
+                    return (title.range(of: searchText) != nil)
+                }
+                return false
+            })
+        }
+        self.tableView.reloadData()
+        self.collectionView.reloadData()
+    }
+    
+    
     func loadFromNetwork(){
         let apiKey = "cee559a4a6b70debdcf335be6e319ce0"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
@@ -208,7 +215,7 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
                     SVProgressHUD.dismiss()
                     
                     self.movieDictionary = dataDictionary["results"] as? [NSDictionary]
-                    //                    print(dataDictionary)
+                    self.filteredMovies = self.movieDictionary
                     if self.tableView.isHidden {
                         self.collectionView.reloadData()
                     }else{
@@ -221,11 +228,31 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         task.resume()
     }
     
+    //animation for fading in posters
+    func fadeInImageAtView(url: URL, posterImageView: UIImageView) -> Void{
+        let imageRequest = URLRequest(url: url)
+        posterImageView.setImageWith(imageRequest, placeholderImage: nil, success: { (imageRequest, response, image) in
+            if response != nil{
+                posterImageView.alpha = 0
+                posterImageView.image = image
+                UIView.animate(withDuration: 0.3, animations: {
+                    posterImageView.alpha = 1
+                })
+            }else{
+                posterImageView.image = image
+            }
+        }) { (imageRequest, response, error) in
+            print(error)
+        }
+    }
+    
+    // called on refresh event
     func refreshContent(_ refreshControl: UIRefreshControl){
         self.loadFromNetwork()
         refreshControl.endRefreshing()
     }
     
+    //called when view is switched
     func switchView(_ sender: Any) {
         if self.tableView.isHidden {
             self.tableView.isHidden = false
